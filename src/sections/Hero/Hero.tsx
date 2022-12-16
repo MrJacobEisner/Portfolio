464914cb1313d9539f036fd33370322c5cb85783
MrJacobEisner <mrjeisner@gmail.com>
2022-12-16 16:19:42 -0700

    WIP Css applied

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -8,27 +8,31 @@ function Hero(props: any) {
                 <h1>Let's get the</h1>
                 <h1>
                     ball rolling
-                    <span>.</span>
+                    <h1>
+                        <span>.</span>
+                    </h1>
                 </h1>
             </div>
-            <div className="Socials">
-                {/* TODO -- ADD ICONS + ACTIONS */}
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <div className="ContactInfo">
-                <div className="Phone">
-                    <h3>Phone</h3>
-                    <h4>403-555-5555</h4>
+            <div className="content">
+                <div className="Socials">
+                    {/* TODO -- ADD ICONS + ACTIONS */}
+                    <div></div>
+                    <div></div>
+                    <div></div>
                 </div>
-                <div className="Email">
-                    <h3>Email</h3>
-                    <h4>jeisner@ucsb.edu</h4>
-                </div>
-                <div className="Location">
-                    <h3>Location</h3>
-                    <h4>6750 El Colegio Rd Santa Barbara, CA Apt 247</h4>
+                <div className="ContactInfo">
+                    <div className="Phone">
+                        <h3>Phone</h3>
+                        <h4>403-555-5555</h4>
+                    </div>
+                    <div className="Email">
+                        <h3>Email</h3>
+                        <h4>jeisner@ucsb.edu</h4>
+                    </div>
+                    <div className="Location">
+                        <h3>Location</h3>
+                        <h4>6750 El Colegio Rd Santa Barbara, CA Apt 247</h4>
+                    </div>
                 </div>
             </div>
         </div>
