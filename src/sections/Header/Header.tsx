c730bd81a392224048125d65bac2b9b4b27b201b
MrJacobEisner <mrjeisner@gmail.com>
2022-12-16 14:47:17 -0700

    WIP Restructure

---
+++ src/sections/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.scss";
+
+function Header(props: any) {
+    return (
+        <div className="Header">
+            <div className="Logo">
+                <h4>{props.Logo}</h4>
+            </div>
+            <div className="Navigation">{props.children}</div>
+        </div>
+    );
+}
+
+export default Header;
